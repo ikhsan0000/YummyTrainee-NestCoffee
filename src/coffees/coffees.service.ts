fb5ef0787a4de408f0d8f359568e91b2ca363b0c
ikhsan0000 <ikhsanfirdauz000@gmail.com>
2021-12-14 16:10:43 +0700

    event entity payload property type fixed

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable, NotFoundException } from '@nestjs/comm
 import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 import { Connection, Model } from 'mongoose';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
-// import { Event } from 'src/events/entities/event.entity';
+import { Event } from 'src/events/entities/event.entity';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
 
@@ -11,8 +11,8 @@ export class CoffeesService {
 
     constructor(
         @InjectModel(Coffee.name) private readonly coffeeModel: Model<Coffee>,
-        // @InjectConnection() private readonly connection: Connection,
-        // @InjectModel(Event.name) private readonly eventModel: Model<Event>
+        @InjectConnection() private readonly connection: Connection,
+        @InjectModel(Event.name) private readonly eventModel: Model<Event>
     ){}
 
     async findAll(paginationQuery: PaginationQueryDto)
@@ -64,30 +64,30 @@ export class CoffeesService {
         return deleteCoffee;
     }
 
-    // async recommendationCoffee(coffee: Coffee)
-    // {
-    //     const session = await this.connection.startSession();
-    //     session.startTransaction();
+    async recommendationCoffee(coffee: Coffee)
+    {
+        const session = await this.connection.startSession();
+        session.startTransaction();
 
-    //     try {
-    //         coffee.recommendations++;
+        try {
+            coffee.recommendations++;
 
-    //         const reccomendEvent = new this.eventModel({
-    //             name: 'recommend_coffee',
-    //             type: 'coffee',
-    //             payload: { coffeeId: coffee.id }
-    //         });
-    //         await reccomendEvent.save({session});
-    //         await coffee.save({session});
+            const reccomendEvent = new this.eventModel({
+                name: 'recommend_coffee',
+                type: 'coffee',
+                payload: { coffeeId: coffee.id }
+            });
+            await reccomendEvent.save({session});
+            await coffee.save({session});
 
-    //         await session.commitTransaction();
+            await session.commitTransaction();
             
-    //     } catch (error) {
-    //         await session.abortTransaction();
-    //     }
-    //     finally {
-    //         session.endSession();
-    //     }
-    // }
+        } catch (error) {
+            await session.abortTransaction();
+        }
+        finally {
+            session.endSession();
+        }
+    }
 
 }
